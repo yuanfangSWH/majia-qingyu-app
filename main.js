76f03ac68046b39e0ea7492e42c79a068c58a9ad
大元芳 <112684202+yuanfangSWH@users.noreply.github.com>
2022-09-06 14:08:30 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+// VUEX
+import store from './store'
+Vue.prototype.$store = store
+//API
+import api from './common/api/api.js';
+Vue.prototype.$api = api
+//通用头部标题栏
+import Navigation from './pages/navigation/navigation.vue'
+Vue.component('Navigation',Navigation)
+
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
